b02b32774069959dcd916c9a71f3663fc5da9735
jcbarthol <78051010+jcbarthol@users.noreply.github.com>
2021-04-11 21:01:23 -0400

    Added create hotel form

--- hotelform.blade.php
+++ hotelform.blade.php
@@ -3,7 +3,20 @@
 
 <body>
 
+<?php
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
+        $hName = trim(strtolower($_POST['hName']));
+        $a1 = trim(strtolower($_POST['a1']));
+        $hCity = trim(strtolower($_POST['hCity']));
+        $hState = trim(strtolower($_POST['hState']));
+        $hZip = trim(strtolower($_POST['hZip']));
+        $hDesc = trim(strtolower($_POST['hDesc']));
+        $hImage = trim(strtolower($_POST['hImage']));
+
+    }
+
+?>
     <form name="newHotel" id="newHotel" method="post" action="/hotels/store">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <br><br>
@@ -12,9 +25,6 @@
         <br><br>
         <label for="a1">Address 1:</label>
         <input type="text" name="a1" id="a1" size="50" maxlength="100" placeholder="Address 1">
-        <br><br>
-        <label for="a2">Address 2:</label>
-        <input type="text" name="a2" id="a2" size="50" maxlength="100" placeholder="Address 2">
         <br><br>
         <label for="hCity">City:</label>
         <input type="text" name="hCity" id="hCity" size="50" maxlength="100" placeholder="City">
